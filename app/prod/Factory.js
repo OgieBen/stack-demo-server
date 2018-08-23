bcfb8c19307435436e9385f59e71a540e4bfe534
Ogie Ben <bensoft2k5@gmail.com>
2018-08-23 23:36:28 +0100

    splitted comments into two categories

--- app/prod/Factory.js
+++ app/prod/Factory.js
@@ -2,7 +2,7 @@
 import { DBConnector } from './data/db/DBConnector';
 import { Auth } from './auth/Auth';
 import { Answer } from './data/model/Answer';
-import { Comment } from './data/model/Comment';
+import { Comment } from './data/model/QComment';
 
 export class Factory {
 
