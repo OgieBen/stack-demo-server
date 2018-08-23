d90ed60aac587aced18fed5e18091b5705fc158c
Ogie Ben <bensoft2k5@gmail.com>
2018-08-24 00:07:09 +0100

    added question routes to api folder

---
+++ app/prod/api/questions.js
@@ -0,0 +1,50 @@
+import  { Router } from 'express';
+import { Factory } from '../Factory';
+
+let router = Router();
+
+
+let factory = new Factory();
+let auth = factory.getAuth();
+
+
+/**
+ * Fetches all questions that have been asked on the platform
+ * 
+ * @returns {JSON} List
+ */
+router.get('/v1/questions', (req, res, next) => {
+
+});
+
+/**
+ * Fetch a specific question
+ * This should come with all the answers
+ * provided so far for the question.
+ * 
+ * @method POST
+ * @param {Integer} id
+ * 
+ * @returns {JSON} Question
+ */
+router.get('/v1/questions/{id}', (req, res, next) => {
+
+});
+
+/**
+ * Adds a question the platform
+ */
+router.post('/v1/questions', (req, res, next) => {
+
+});
+
+/**
+ * Removes a question from the platform
+ */
+router.delete('/v1/questions/{id}', (req, res, next) => {
+
+});
+
+
+
+module.exports = router;
